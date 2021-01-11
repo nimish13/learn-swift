6b3e589fd815d760c996b453ff953d0297eff94c
Nimish Gupta <nimish.gupta@vinsol.com>
2021-01-11 19:01:15 +0530

    Print n prime numbers

---
+++ prime_number.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+func isPrime(_ number: Int) -> Bool {
+    let primeCheckLimit = Int(ceil(Double(number).squareRoot()))
+    if number < 2 { return false }
+    return !((2..<primeCheckLimit).contains { current in return number % current == 0 })
+
+}
+
+
+func printPrimeNumbers(_ numberCount: Int) {
+    for number in 1...numberCount {
+        if(isPrime(number)) {
+            print(number)
+        }
+    }
+}
+
+printPrimeNumbers(100)
